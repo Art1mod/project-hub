27b03794e53381740514f61dcc96c0fb852fdf31
Art1mod <artmod2112@gmail.com>
2026-09-18 22:50:03 +0200

    feat: add a organization package

--- backend/project-hub/src/app.module.ts
+++ backend/project-hub/src/app.module.ts
@@ -4,12 +4,13 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { OrganizationsModule } from './organizations/organizations.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule, 
-    PrismaModule
+    PrismaModule, OrganizationsModule
   ],
   controllers: [AppController],
   providers: [AppService],
