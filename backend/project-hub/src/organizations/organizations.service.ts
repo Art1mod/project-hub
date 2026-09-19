3b8243e5b0d733e0b335b855e10a156fc7cd67b7
Art1mod <artmod2112@gmail.com>
2026-09-19 13:24:52 +0200

    feat: implement organization creation and retrieval
    - Created createOrganization service using Prisma nested writes to securely assign the OWNER role.
    - Added getUserOrganizations and getOrganizationById services to fetch organizations scoped to user memberships.

--- backend/project-hub/src/organizations/organizations.service.ts
+++ backend/project-hub/src/organizations/organizations.service.ts
@@ -1,4 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class OrganizationsService {}
+export class OrganizationsService {
+    constructor(private prisma: PrismaService) {}
+
+    async createOrganization(userId:string, name:string) {
+        return await this.prisma.organization.create({
+                data: {
+                    name,
+                    memberships: {
+                        create: {
+                            userId,
+                            role: 'OWNER'
+                        }
+                    }
+                }
+        });
+    }
+
+    async getUserOrganizations(userId: string) {
+        return await this.prisma.organization.findMany({ 
+            where: {
+                memberships : {
+                    some: {
+                        userId: userId,
+                    }
+                }            
+            },
+            include : {
+                memberships: {
+                    where: {
+                        userId: userId
+                    },
+                    select: {
+                        role:true,
+                    }
+                }
+            }
+        });
+    }
+
+    async getOrganizationById(userId: string, orgId: string) {
+        const organization = await this.prisma.organization.findFirst({
+            where: {
+                id: orgId,
+                memberships: {
+                    some: {
+                        userId: userId,
+                    }
+                }
+            },
+            include: {
+                    memberships: {
+                        where: {
+                            userId: userId,    
+                        },
+                        select: {
+                            role: true,
+                        }
+                    }
+                }
+        });
+
+        if (!organization) throw new NotFoundException("Organization not found or access denied");
+
+        return organization;
+    }
+}
