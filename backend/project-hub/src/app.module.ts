dc4e48d3c78601fea5a981ccadd3fcba578b6fbf
Art1mod <artmod2112@gmail.com>
2026-09-20 14:36:28 +0200

    feat(projects): add a project package

--- backend/project-hub/src/app.module.ts
+++ backend/project-hub/src/app.module.ts
@@ -5,12 +5,13 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { OrganizationsModule } from './organizations/organizations.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule, 
-    PrismaModule, OrganizationsModule
+    PrismaModule, OrganizationsModule, ProjectsModule
   ],
   controllers: [AppController],
   providers: [AppService],
