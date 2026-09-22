da87714ed34cafac520243e55c2122f734ab6024
Art1mod <artmod2112@gmail.com>
2026-09-22 18:40:37 +0200

    feat(projects): implement update and delete service logic
    - Added updateProjectById and deleteProjectById methods to ProjectsService.
    - Implemented manual RBAC via Prisma to ensure only OWNER or ADMIN roles can modify or delete projects.

--- backend/project-hub/src/projects/projects.service.ts
+++ backend/project-hub/src/projects/projects.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { OrganizationsService } from '../organizations/organizations.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { PrismaService } from '../prisma/prisma.service';
+import { UpdateProjectDto } from './dto/update-project.dto';
 
 @Injectable()
 export class ProjectsService {
@@ -49,5 +50,55 @@ export class ProjectsService {
         if (!project) throw new NotFoundException("Project not found or access denied");
 
         return project;
+    }
+
+    async deleteProjectById(userId: string, projectId: string) {
+        const project = await this.prisma.project.findFirst({
+            where: {
+                id: projectId,
+                organization: {
+                    memberships: {
+                        some: {
+                            userId: userId,
+                            role: { in: ['OWNER', 'ADMIN'] }
+                        }
+                    }
+                }
+            }
+        });
+
+        if (!project) throw new NotFoundException("Project not found or access denied");   
+
+        return await this.prisma.project.delete({where: {
+            id: projectId,
+        }});
+    }
+
+    async updateProjectById(userId: string, projectId: string, newData: UpdateProjectDto) {
+        const project = await this.prisma.project.findFirst({
+            where: {
+                id: projectId,
+                organization: {
+                    memberships: {
+                        some: {
+                            userId: userId,
+                            role: { in: ['OWNER', 'ADMIN'] }
+                        }
+                    }
+                }
+            }
+        });
+
+        if (!project) throw new NotFoundException("Project not found or access denied");   
+
+        return await this.prisma.project.update({
+            where: {
+                    id: project.id
+                },
+            data: {
+                name: newData.name,
+                description: newData.description
+            }
+            });
     }
 }
