04c62d3114db21dbdc7ee4d3b02e440f2173943a
Art1mod <artmod2112@gmail.com>
2026-09-24 19:44:53 +0200

    fix(tasks): add @IsOptional() decorator for status property in the dto

--- backend/project-hub/src/tasks/dto/create-task.dto.ts
+++ backend/project-hub/src/tasks/dto/create-task.dto.ts
@@ -2,7 +2,6 @@ import { Priority, Status } from "@prisma/client";
 import { IsEnum, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class CreateTaskDto {
-
     @IsString()
     @IsNotEmpty()
     readonly name: string;
@@ -12,8 +11,10 @@ export class CreateTaskDto {
     readonly description?: string;
 
     @IsEnum(Status)
+    @IsOptional()
     readonly status: Status = "TODO";
 
     @IsEnum(Priority)
+    @IsOptional()
     readonly priority: Priority = "MEDIUM"; 
 }
