3c020976de31b934bc6c0cb513ab5f98e6f23524
Art1mod <artmod2112@gmail.com>
2026-09-26 20:57:17 +0200

    feat(organizations): map PATCH route for updating organizations
    - Added PATCH /organizations/:id endpoint to OrganizationsController.
    - Wired up UpdateOrganizationDto for payload validation and @CurrentUser() for secure routing to the service layer.

--- backend/project-hub/src/organizations/organizations.controller.ts
+++ backend/project-hub/src/organizations/organizations.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, Body, UseGuards, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Get, Param, Patch, Delete } from '@nestjs/common';
 import { OrganizationsService } from './organizations.service';
 import { CreateOrganizationDto } from './dto/create-organization.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt_auth.guard';
 import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import { UpdateOrganizationDto } from './dto/update-organization.dto';
 
 @UseGuards(JwtAuthGuard)
 @Controller('organizations')
@@ -25,5 +26,22 @@ export class OrganizationsController {
   getOrganizationById(@Param('id') orgId:string, 
   @CurrentUser() user: {userId: string}) {
     return this.organizationsService.getOrganizationById(user.userId, orgId);
+  }
+
+  @Delete(':id')
+  deleteOrganizationById(
+    @Param('id') orgId:string,
+    @CurrentUser() user: {userId: string},  
+  ) {
+    return this.organizationsService.deleteOrganizationById(user.userId, orgId);
+  }
+
+  @Patch(':id')
+  updateOrganizationById(
+    @Param('id') orgId:string,
+    @CurrentUser() user: {userId: string},
+    @Body() body: UpdateOrganizationDto,
+  ) {
+    return this.organizationsService.updateOrganizationById(user.userId, orgId, body);
   }
 }
